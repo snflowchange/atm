0664f5f767ca5095f508fefddd755f2ef08a0645
Sverre Nygaard <sn@flowchange.no>
2025-11-18 19:45:03 +0100

    fixed things

---
+++ autocraft.lua
@@ -0,0 +1,25 @@
+function wrapPs(peripheralName)
+    local periTab = {}
+    local sideTab = {}
+
+    if not peripheralName then
+        print("Error: No peripheral name")
+        return nil
+    end
+
+    for _, side in ipairs(peripheral.getNames()) do
+        if string.lower(peripheral.getType(side)) == string.lower(peripheralName) then
+            table.insert(periTab, peripheral.wrap(side))
+            table.insert(sideTab, side)
+        end
+    end
+
+    if #periTab > 0 then
+        print("Found peripheral")
+        return periTab, sideTab
+    else
+        return nil
+    end
+end
+
+x = wrapPs("rs_bridge")
