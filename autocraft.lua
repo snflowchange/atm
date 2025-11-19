813b2e29c20c03d0552f1f548b4fe2f394816d9f
Sverre Nygaard <sn@flowchange.no>
2025-11-19 20:32:58 +0100

    neger

--- autocraft.lua
+++ autocraft.lua
@@ -26,4 +26,42 @@ label = "NegerCraft"
 
 rs = wrapPs("rs_bridge")[1]
 
-print(rs.isOnline())
+mon = wrapPs("monitor")[1]
+
+rsItems = {
+    {"Oak Planks", "minecraft:oak_planks", 100 }
+}
+
+function CenterT(text, line, txtback, txtcolor, pos)
+    local monX, monY = mon.getSize()
+    mon.setBackGroundColor(txtback)
+    mon.setTextColor(txtcolor)
+    
+    local length = #text
+    local x = math.floor((monX-length)/2)
+    if pos == "head" then
+        mon.setCursorPos(x+1,line)
+    elseif pos == "left" then
+        mon.setCursorPos(2, line)
+    elseif pos == "right" then
+        mon.setCursorPos(monX - length, line)
+    end
+
+    mon.write(text)
+end
+
+function clearScreen()
+    mon.setBackgroundColor(colors.black)
+    mon.clear()
+    mon.setCursorPos(1,1)
+    CenterT(label, 1, colors.black, colors.white, "head")
+end
+
+function checkRs(checkName, displayName, low)
+    local craftables = rs.getCraftables()
+    print(craftables)
+end
+
+print(checkRs("test", "test", "test"))
+
+
